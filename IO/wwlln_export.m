51ece2239f0355d69694b74dfc3629e5211e8c3c
Michael Hutchins <mlhutch@uw.edu>
2012-08-15 11:13:01 -0700

    Updated wwlln_export to handle odd sized files.

--- IO/wwlln_export.m
+++ IO/wwlln_export.m
@@ -1,11 +1,17 @@
 %Export data to file
-function power_export(data,filename)
+function wwlln_export(data,filename)
 
 
 fid=fopen(filename,'wt');
+format='%g/%g/%g,%02g:%02g:%09f, % 08.4f, % 09.4f,% 05.1f, %g, %.2f %.2f %g\n';
+if size(data,2)~=13
+    format=sprintf('%%g/%%g/%%g,%%02g:%%02g:%%09f, %% 08.4f, %% 09.4f,%% 05.1f, %%g%s\n',...
+        repmat(', %.2f',1,size(data,2)-10));
+end
+
 for i=1:size(data,1);
-   
-fprintf(fid,'%g/%g/%g,%02g:%02g:%09f, % 08.4f, % 09.4f,% 05.1f, %g, %.2f %.2f %g\n',data(i,1:13));
+      
+fprintf(fid,format,data(i,:));
 
 end
 fclose(fid);
