b7a7ca4ddbd5d99c831d96d5ae347d788642d9b0
Michael Hutchins <mlhutch@uw.edu>
2012-11-09 14:52:25 -0800

    Updated lookup_import to be able to read in the new format of the high resolution lookup files which includes the station information (name and location, output as a structure). Still backward compatible with old lookup format.

--- IO/lookup_import.m
+++ IO/lookup_import.m
@@ -1,15 +1,22 @@
-function [lookup,div]=lookup_import(filename)
+function [lookup,div,station]=lookup_import(filename)
 %lookup_import Loads the lookup text file
     fid=fopen(filename);
     fend=feof(fid);
     s=fgets(fid);
     index=1;
     index_stat=1;
+    
     while fend==0
        A=sscanf(s,'%g\t');
        if isempty(A) | isnan(A); 
-           index_stat=index_stat+1;
-           index=1;
+           A=sscanf(s,'%s %g %g');
+           if isempty(A)
+               index_stat=index_stat+1;
+               index=1;
+           else
+               station.loc(index_stat,:) = A(end-1:end);
+               station.name{index_stat} = char(A(1:end-2))';
+           end
        else
            lookup(index,:,index_stat)=A;
            index=index+1; 
