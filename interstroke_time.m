21fb8491bdfdff3e7f01c63529a5d022d0d23528
Michael Hutchins <mlhutch@uw.edu>
2012-11-09 10:21:11 -0800

    interstroke_time is now working as expected. Currently in the process of optimizing both the code itself and the default values.

--- interstroke_time.m
+++ interstroke_time.m
@@ -6,6 +6,7 @@ function [ interstroke ] = interstroke_time( data , resTime, resDist )
     resTimeDefault = 1;
     resDistDefault = 25;
     
+    % Set defaults if not specified
     switch nargin
         case 1
             resTime = resTimeDefault;
@@ -18,41 +19,62 @@ function [ interstroke ] = interstroke_time( data , resTime, resDist )
     bin = 10000;
     binCount = floor(size(data,1)/bin);
         
+    % Go through each bin
     for k = 1 : binCount
             
+        % Select the data in bin k
         if k == binCount
             timeData = data(bin*(k-1)+1 : end,:);
         else
             timeData = data(bin*(k-1)+1 : bin*k,:);
         end
-                    
+        
+        % Convert datevec formatted time to seconds since start of bin
         time = (datenum(timeData(:,1:6)) - min(datenum(timeData(:,1:6))));
         time = time*24*3600;
 
-        
+        % Initialize arrays to store interstroke data
         interTime = zeros(size(timeData,1),1);
         interDist = interTime;
-
+        interEnergy = interTime;
+        % Process each stroke
         for j = 1 : size(timeData,1);
 
+            % Select strokes from resTime before to stroke time and within
+            % sensible distances (km/50 ~ 2*degree box)
             loc = time < time(j) &...
-                  time >= time(j) - resTime;
+                  time >= time(j) - resTime &...
+                  timeData(:,7) >= timeData(j,7) - resDist/50 &...
+                  timeData(:,7) <= timeData(j,7) + resDist/50 &...
+                  timeData(:,8) >= timeData(j,8) - resDist/50 &...
+                  timeData(:,8) <= timeData(j,8) + resDist/50;
 
+            % Check to see if there is a matching stroke 
             if sum(loc)>0
 
+                % Get time from previous stroke to current stroke
                 prevTime = time(j) - time(loc);
-
+                prevEnergy = timeData(loc,11);
+                
+                % Get distnce between the strokes
                 dist = vdist(timeData(loc,7),timeData(loc,8),timeData(j,7),timeData(j,8))./1000;
 
-                loc = dist <= limitDist;
+                % Find strokes within resDist km
+                locDist = dist <= resDist;
 
-                if sum(loc)>0
+                % Check to see if there is a stroke
+                if sum(locDist)>0
 
-                    locMin = abs(prevTime) == min(abs(prevTime));
+                    % Find the closest stroke in time
+                    locMin = abs(prevTime) == min(abs(prevTime(locDist)));
+                    
+                    
+                    % Store that strokes time and distance
                     if sum(locMin) == 1
 
                         interTime(j) = prevTime(locMin);
                         interDist(j) = dist(locMin);
+                        interEnergy(j) = prevEnergy(locMin);
 
                     end
 
@@ -62,11 +84,12 @@ function [ interstroke ] = interstroke_time( data , resTime, resDist )
 
         end
             
+        % Store time and distance from each bin in interstroke
         if k == 1
-            interstroke = [interTime,interDist];
+            interstroke = [interTime,interDist,interEnergy];
         else
             
-            interstroke = [interstroke ; [interTime,interDist]];
+            interstroke = [interstroke ; [interTime,interDist,interEnergy]];
         end
         
         
