42f7e085288b9d9d700a0ec1a195cf7c77220805
Michael Hutchins <mlhutch@uw.edu>
2013-01-31 10:28:17 -0800

    Added in gzipping of the old Rfiles, and creating .mat files for the new daily Rfiles (10x faster loading). Hopefully there is space on /wd2/.

--- IO/r_reformat.m
+++ IO/r_reformat.m
@@ -1,6 +1,6 @@
 % Traverse Rfile tree and reformat + reorganize
 
-dateRange = datenum([2005,01,01]) : datenum([2013,1,30]);
+dateRange = datenum([2005,01,01]) : datenum([2013,2,1]);
 
 for i = 1 : length(dateRange);
     date = datevec(dateRange(i));
@@ -13,5 +13,15 @@ for i = 1 : length(dateRange);
     cd(r_path);
     eval(sprintf('!cat %s >> %s',r_day,r_name));
     eval(sprintf('!mv %s %s',r_name,r_dir));
+    
+    dateNext = datevec(dateRange(i+1));
+    if date(2) ~= dateNext(2)
+        !gzip R* &
+    end
+    
+    cd(r_dir)
+    data = r_import(r_name);
+    save(r_name,'data');
+    clear data
     
 end
