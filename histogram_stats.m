f43f04041e073e8279712404aa86fa05c0fb799f
Michael Hutchins <mlhutch@uw.edu>
2013-04-23 11:04:51 -0700

    Added histogram_stats which gets the statistics of a histogram given it's base vector. Probably needs serious optimization.

---
+++ histogram_stats.m
@@ -0,0 +1,22 @@
+function [ histMean, histMedian, histStd, histMad ] = histogram_stats(histogram, base)
+% HISTOGRAM_STATS returns the mean, median, standard deviation, and median
+%   absolute deviation for an input histogram given the base vector.
+
+    vector = zeros(sum(histogram),1);
+    index = 1;
+    for i = 1 : length(base)
+        
+        nextIndex = index+histogram(i);
+        
+        vector(index : nextIndex - 1) = repmat(base(i),histogram(i),1);
+        
+        index = nextIndex;
+    end
+
+    histMean = mean(vector);
+    histMedian = median(vector);
+    histStd = std(vector);
+    histMad = mad(vector);
+
+end
+
