c46b9a5831bbfc700e76f576b0e7e00a5399c295
Michael Hutchins <mlhutch@uw.edu>
2013-05-01 13:40:06 -0700

    Added git_hash to get the current commit has for the current or specified directory.

---
+++ git_hash.m
@@ -0,0 +1,17 @@
+function [ hash ] = git_hash(directory)
+%git_hash returns the git hash of the current commit for the current directory
+%   The optinal argument directory will get the hash for the specified
+%   location
+
+    switch nargin
+        case 0
+            directory = pwd;
+    end
+
+    current_directory = pwd;
+    cd(directory)
+    !git rev-list --max-count=1 HEAD
+    cd(current_directory)
+
+end
+
