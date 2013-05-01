791c98ceffb32610a6c3f6ce060bea7e1c7e93e7
Michael Hutchins <mlhutch@uw.edu>
2013-05-01 12:14:23 -0700

    Added function to calculate dB from a field measurement. The reference field is an optional argument.

---
+++ field_to_db.m
@@ -0,0 +1,12 @@
+function [ db ] = field_to_db( field, reference)
+%field_to_db converts field measurements to decibels
+
+    switch nargin
+        case 1
+            reference = 1;
+    end
+
+    db = 20 .* log10(field ./ reference);
+
+end
+
