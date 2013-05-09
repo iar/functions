00cdbf7d366e2ee25c6a2eaebe92b4a1190179eb
Michael Hutchins <mlhutch@uw.edu>
2013-05-09 15:05:07 -0700

    Vectorized find_array.

--- find_array.m
+++ find_array.m
@@ -1,13 +1,17 @@
 function [ location ] = find_array( value, array )
 %FIND_ARRAY finds the index location of VALUE in ARRAY for 2D arrays.
 
-    near = min(abs(array(:) - value));
-    nearest = array(abs(array(:) - value) == near);
+    location = zeros(length(value),2);
 
-    index = find(array == nearest);
-    
-    location(2) = ceil(index/size(array,1));
-    location(1) = mod(index,size(array,1));
-    
+    for i = 1 : length(value);
+
+        near = min(abs(array(:) - value));
+        nearest = array(abs(array(:) - value) == near);
+
+        index = find(array == nearest);
+
+        location(i,2) = ceil(index/size(array,1));
+        location(i,1) = mod(index,size(array,1));
+
+    end
 end
-
