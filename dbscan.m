90d9144d4eaab678adbfe6050c4f4037ef779242
Michael Hutchins <mlhutch@uw.edu>
2013-06-21 13:44:34 -0700

    Start of my DBSCAN function.

---
+++ dbscan.m
@@ -0,0 +1,9 @@
+function [ output_args ] = dbscan( D, eps, minPts )
+%DBSCAN clustering of data D into clusters with distance less than eps and at least minPts members.
+% http://en.wikipedia.org/wiki/DBSCAN
+%
+%	Written by: Michael Hutchins
+
+
+end
+
