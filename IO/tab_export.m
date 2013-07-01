e7f9d36ef04a0f89262780790bf020e30f467b8d
Michael Hutchins <mlhutch@uw.edu>
2013-07-01 11:14:58 -0700

    Set tab_export to be able to export as integers.

--- IO/tab_export.m
+++ IO/tab_export.m
@@ -1,12 +1,28 @@
-function tab_export(filename,data)
+function tab_export(filename,data,format)
 %tab_export(filename,data)
     %exports data into a the tab delimited filename. 
     %Michael Hutchins - 9/12/11 
+	
+	switch nargin
+		case 2
+			dataType = '%f\t';
+		case 3
+			if strcmp(format,'float')
+				dataType = '%f\t';
+			elseif strcmp(format,'int')
+				dataType = '%d\t';
+			elseif strcmp(format,'double')
+				dataType = '%g\t';
+			else
+				error('Unknown format: float, int, or double')
+			end
+	end
+	
     
     columns=size(data,2);
     fid=fopen(filename,'wt');
     for i=1:size(data,1);
-        fprintf(fid,sprintf('%s\n',repmat('%f\t',1,columns)),data(i,:));
+        fprintf(fid,sprintf('%s\n',repmat(dataType,1,columns)),data(i,:));
     end
     fclose(fid);
 end
