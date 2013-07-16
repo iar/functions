06e3be9bbe59cee3a93586bcab7c99402bf40725
Michael Hutchins <mlhutch@uw.edu>
2013-07-16 15:30:16 -0700

    Fixed caliper for handling edge cases.

--- caliper.m
+++ caliper.m
@@ -12,68 +12,95 @@ function [a,b]=caliper(X,Y,varargin)
 
 %   Future improvements - make accurate over spherical geometry
 
-Options=varargin;
-stepSize=5;
-centering='mean';
-Degree=false;
-for i=1:length(Options)
-    if strncmp(Options{i},'stepSize',3)
-        stepSize=Options{i+1};
-    elseif strncmp(Options{i},'Median',6)
-        centering='median';
-    elseif strncmp(Options{i},'Mean',4)
-        centering='mean';
-    elseif strncmp(Options{i},'RMS',3)
-        centering='rms';
-    elseif strncmp(Options{i},'Degree',6)
-        Degree=true;
-    end
-end
+%% Set Run Options
 
-if length(X)<=1
-    error('Not Enough Data')
-end
+	Options=varargin;
+	stepSize=5;
+	centering='mean';
+	Degree=false;
+	for i=1:length(Options)
+		if strncmp(Options{i},'stepSize',3)
+			stepSize=Options{i+1};
+		elseif strncmp(Options{i},'Median',6)
+			centering='median';
+		elseif strncmp(Options{i},'Mean',4)
+			centering='mean';
+		elseif strncmp(Options{i},'RMS',3)
+			centering='rms';
+		elseif strncmp(Options{i},'Degree',6)
+			Degree=true;
+		end
+	end
 
-if Degree && max(abs(Y))>90
+%% Check Inputs
+	
+	if length(X)<=1
+		error('Not Enough Data')
+	end
+
+	if Degree && max(abs(Y))>90
+
+		Xnew = Y;
+		Ynew = X;
+		X = Xnew;
+		Y = Ynew;
+
+	end
+
+	if Degree && (range(X) > 300 || range(Y) > 300);
+		X = wrapTo360(X);
+		Y = wrapTo360(Y);
+	end
+
+
+	X = X(:);
+	Y = Y(:);
+
+%% Center inputs
+	
+	eval(sprintf('center=[%s(X),%s(Y)];',centering,centering));
+
+	x=X-center(1);
+	y=Y-center(2);
+
+%% Initialize rotation arrays
+	
+	h=zeros(180/stepSize,1);
+	w=h;
+
+%% Rotate pointsand get height and width
+	
+	for i=1:180/stepSize;
+		theta=i*stepSize;
+		R=[cosd(theta) -sind(theta); sind(theta) cosd(theta)];
+		rot=R*[x,y]';
+		xRot=rot(1,:);
+		yRot=rot(2,:);
+
+		if Degree
+			h(i)=vdist(max(yRot)+center(2),center(1),min(yRot)+center(2),center(1))/1000;
+			w(i)=vdist(center(2),max(xRot)+center(1),center(2),min(xRot)+center(1))/1000;
+		else
+			h(i)=max(yRot)-min(yRot);
+			w(i)=max(xRot)-min(xRot);
+		end
+	end
     
-    Xnew = Y;
-    Ynew = X;
-    X = Xnew;
-    Y = Ynew;
-    
-end
-    
-X = X(:);
-Y = Y(:);
+%% Find rotation that minimizes the area
 
-eval(sprintf('center=[%s(X),%s(Y)];',centering,centering));
+	minAngle=find(h.*w==min(h.*w));
+	minDim=[w(minAngle),h(minAngle)];
 
-x=X-center(1);
-y=Y-center(2);
+	a=minDim(minDim(:,1)==max(minDim(:,1)),1);
+	b=minDim(minDim(:,1)==max(minDim(:,1)),2);
 
-h=zeros(180/stepSize,1);
-w=h;
+%% Ensure a is set to semi-major axis
 
-for i=1:180/stepSize;
-    theta=i*stepSize;
-    R=[cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-    rot=R*[x,y]';
-    xRot=rot(1,:);
-    yRot=rot(2,:);
-    
-    if Degree
-        h(i)=vdist(max(yRot)+center(2),center(1),min(yRot)+center(2),center(1))/1000;
-        w(i)=vdist(center(2),max(xRot)+center(1),center(2),min(xRot)+center(1))/1000;
-    else
-        h(i)=max(yRot)-min(yRot);
-        w(i)=max(xRot)-min(xRot);
-    end
-end
-    
-minAngle=find(h.*w==min(h.*w));
-minDim=[w(minAngle),h(minAngle)];
-
-a=minDim(minDim(:,1)==max(minDim(:,1)),1);
-b=minDim(minDim(:,1)==max(minDim(:,1)),2);
+	if b > a
+		a1 = a;
+		b1 = b;
+		a = b1;
+		b = a1;
+	end
     
 end
--- dbscan.m
+++ dbscan.m
@@ -1,5 +1,5 @@
 function [ clusters ] = dbscan( D, eps, minPts )
-%DBSCAN clustering of data D into clusters with distance less than eps and at least minPts members.
+%DBSCAN(D, eps, minPts) clustering of data D into clusters with distance less than eps and at least minPts members.
 % http://en.wikipedia.org/wiki/DBSCAN
 %
 %	Written by: Michael Hutchins
