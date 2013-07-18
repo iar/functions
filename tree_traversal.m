a179e9c5354adbfe8f3b396ee857535811f58660
Michael Hutchins <mlhutch@uw.edu>
2013-07-18 16:22:17 -0700

    Trying to patch and fix the matlab list method.

--- tree_traversal.m
+++ tree_traversal.m
@@ -9,23 +9,27 @@ function [ groups ] = tree_traversal( tree )
 	
 	groupID = unique(tree(:));
 	
+	% GroupCell is a list of strokes in each group
 	groupCell = cell(length(groupID) + 1,1);
+	
+	% Event cell is a list of groups for each stroke
 	eventCell = cell(size(tree,1),1);
 	
+	% Populate the two cell lists
 	for i = 1 : length(eventCell);
 		
         entries = tree(i,tree(i,:) > 0);
 		
 		for j = 1 : length(entries)
 			
-			entry = entries(j);
+			entry = [entries(j);0];
 			oldEntry = groupCell{entry};
 			
 			groupCell{entry} = [oldEntry,i];
 			
 		end
 		
-		eventCell{i} = entries;
+		eventCell{i} = [entries;zeros(1,length(entries))];
 			
 	end
 	
@@ -33,39 +37,46 @@ function [ groups ] = tree_traversal( tree )
 
     groups = zeros(size(tree,1),1);
 
-    set = false(size(tree,1),1);
 
 	for i = 1 : length(eventCell);
 
         eventGroups = eventCell{i};
 
+		% Need a way to update eventGroups or something
+		% Or remove it or use a sparse matrix
+		
 		if ~isempty(eventGroups)
 
-            if set(i)
-                firstGroup = groups(i);
+            if sum(eventGroups(2,:)) > 0
+				markedGroups = eventGroups(1,eventGroups == 1);
+                firstGroup = markedGroups(end);
             else
-                firstGroup = eventGroups(1);
+                firstGroup = eventGroups(1,1);
             end
 
-            for j = 1 : length(eventGroups)
+            for j = 1 : size(eventGroups,2)
 
 				newGroupMembers = groupCell{eventGroups(j)};
 				
-				for k = 1 : length(newGroupMembers)
+				for k = 1 : size(newGroupMembers,2)
 				
-					memberID = newGroupMembers(k);
+					memberID = newGroupMembers(:,k);
 					
-					if ~set(memberID)
+					if memberID(2) == 0
 						
 						groups(memberID) = firstGroup;
-						set(memberID) = true;
+						
+						newGroupMembers(2,k) = 1;
+						
 					
 					end
 					
 				end
+				
+				groupCell{eventGroups(j)} = newGroupMembers;
 
-            end
-
+			end
+			
 		end
 
 	end	
