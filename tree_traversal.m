1047005561b14e34a44001499b0ab1659b0b3ecd
Michael Hutchins <mlhutch@uw.edu>
2013-07-18 11:29:57 -0700

    Rewrote tree_traversal to be significantly faster by using cell lists instead of a single large array. Still not as fast as desired (1 hour for 1 day worth of a tree).

--- tree_traversal.m
+++ tree_traversal.m
@@ -1,40 +1,74 @@
 function [ groups ] = tree_traversal( tree )
-%TREE_TRAVERSAL(tree) groups tree entries in a left-right manner returning 
-%	a final list of groups across the tree
+%TREE_TRAVERSAL(tree) creates two lists of events per group and groups per event.
+%	The events and groups are traversed to set similar groups together.
 %
 % Created by: Michael Hutchins
 
-	groups = zeros(size(tree,1),1);
+
+	%% Create group location list
 	
-	marked = false(size(tree,1),size(tree,2));
+	groupID = unique(tree(:));
 	
-	for i = 1 : size(tree,1)
+	groupCell = cell(length(groupID),1);
+	eventCell = cell(size(tree,1),1);
+	
+	for i = 1 : length(eventCell);
 		
-		entries = tree(i,tree(i,:) > 0);
-
-		if ~isempty(entries)
+        entries = tree(i,tree(i,:) > 0);
 		
-			if sum(marked(i,:)) > 0
-				first = tree(i,tree(i,:) > 0 & marked(i,:));
-				first = first(end);
-			else
-				first = entries(1);
-			end
-		
-			for j = 1 : length(entries)
-
-				update = tree == entries(j) & ~marked;
-
-				tree(update) = first;
-				marked(update) = true;
-
-			end
-
-			groups(i) = first;
-		
+		for j = 1 : length(entries)
+			
+			entry = entries(j);
+			oldEntry = groupCell{entry};
+			
+			groupCell{entry} = [oldEntry,i];
+			
 		end
 		
+		eventCell{i} = entries;
+			
 	end
+	
+	%% New Method
+
+    groups = zeros(size(tree,1),1);
+
+    set = false(size(tree,1),1);
+
+	for i = 1 : length(eventCell);
+
+        eventGroups = eventCell{i};
+
+        if ~isempty(eventGroups)
+
+            if set(i)
+                firstGroup = groups(i);
+            else
+                firstGroup = eventGroups(1);
+            end
+
+            for j = 1 : length(eventGroups)
+
+				newGroupMembers = groupCell{eventGroups(j)};
+				
+				for k = 1 : length(newGroupMembers)
+				
+					memberID = newGroupMembers(k);
+					
+					if ~set(memberID)
+						
+						groups(memberID) = firstGroup;
+						set(memberID) = true;
+					
+					end
+					
+				end
+
+            end
+
+		end
+
+	end	
 
 end
 
