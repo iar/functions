606f6804910734ee8e053bc96ed94baf2903b642
Michael Hutchins <mlhutch@uw.edu>
2013-07-22 13:00:25 -0700

    Fixed some cases for utc2loc.m.

--- utc2loc.m
+++ utc2loc.m
@@ -1,4 +1,6 @@
 function [local_time]=utc2loc(time,longitude)
+%UTC2LOC(time, longitude) converts the given UTC time to local time based
+%	on the given longitude
 %
 %   Written By:  Michael Hutchins
 
@@ -6,7 +8,7 @@ function [local_time]=utc2loc(time,longitude)
     A=time+longitude./15;
     B=zeros(size(time,1),1);
     for i=1:size(time,1)
-        if A(i)>23
+        if A(i)>=24
             B(i)=A(i)-24;
         elseif A(i)<0
             B(i)=A(i)+24;
