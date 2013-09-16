525eeda1c7eae30f39b957d35cc9ffda1e543842
Michael Hutchins <mlhutch@uw.edu>
2013-09-16 10:41:50 -0700

    Added function to load climate data files.

---
+++ IO/climate_import.m
@@ -0,0 +1,64 @@
+function [storm, strokes] = climate_import(date)
+%Imports a LIS data file:
+%   YYYY,MM,DD,hh,mm,ss,lat,long,evnts,confidence,irradiance,area,duration
+%
+%   Written By:  Michael Hutchins
+
+%% Filepaths
+
+	subdirectory = 'Climate/';
+	prefix = 'climate';
+
+%% Initialize output
+
+	storm = NaN;
+	strokes = NaN;
+	
+%% Check for date specified file
+	if strncmp(class(date),'double',6)
+
+		% Format date into datevec format
+		if length(date) == 1;
+			date=datevec(date);
+			date=date(1:3);
+		elseif length(date)~=3
+			warning('Unknown Input Format');
+		end
+
+		% Generate filename
+		fileName=sprintf('%s%04g%02g%02g.mat',prefix,date(1:3)); 
+
+		% Load dataPath.dat file
+		dataPath = textread('dataPath.dat','%s\n');
+
+		% Check each path for the file
+		for i = 1 : size(dataPath,1);
+			
+			% Generate load name to check
+			path = dataPath{i};
+			fileLoad = sprintf('%s%s/%s',path,subdirectory,fileName);
+			
+			% If found break out of the loop
+			if exist(fileLoad,'file') == 2
+				filename = fileLoad;
+				break;
+			end
+			
+			% If loop ends without finding give error
+			if i == size(dataPath,1)
+				error(sprintf('File %s not found!',fileName));
+			end
+		end
+
+%% Check for filename specified file
+	elseif strmatch(class(date),'char')
+		filename=date;
+%% Error out if not found
+	else
+		error('Unrecognized filename.')
+	end
+
+%% Load file
+	load(filename);
+
+end
