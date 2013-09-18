b5046a71f1aaa075cc6f9384cfe51ac5c491300e
Michael Hutchins <mlhutch@uw.edu>
2013-09-18 13:30:35 -0700

    Updated lis_import.m to use the new import structure (importantly textscan instead of textread).

--- IO/lis_import.m
+++ IO/lis_import.m
@@ -5,30 +5,46 @@ function [flashes, viewtime, groups] = lis_import(date)
 %
 %   Written By:  Michael Hutchins
 
-%% Check for date specified file
+%% Filepaths
+
+	subdirectory = 'LIS';
+	prefix = 'lis';
+	suffix = sprintf('.mat');
+
+%% Initialize output
+
+	flashes = NaN;
+	viewtime = NaN;
+	groups = NaN;
+	
+	%% Check for date specified file
 	if strncmp(class(date),'double',6)
 
 		% Format date into datevec format
 		if length(date) == 1;
 			date=datevec(date);
 			date=date(1:3);
+		elseif length(date) == 6
+			date = date(1:3);
 		elseif length(date)~=3
 			warning('Unknown Input Format');
 		end
 
 		% Generate filename
-		fileName=sprintf('lis%04g%02g%02g.mat',date(1:3)); 
-
+		fileName=sprintf('%s%04g%02g%02g%s',prefix,date(1:3),suffix); 
+		
 		% Load dataPath.dat file
-		dataPath = textread('dataPath.dat','%s\n');
-
+		fid = fopen('dataPath.dat');
+		dataPath = textscan(fid,'%s','Delimiter','\n');
+		dataPath = dataPath{1};
+		
 		% Check each path for the file
 		for i = 1 : size(dataPath,1);
 			
 			% Generate load name to check
 			path = dataPath{i};
-			fileLoad = sprintf('%sLIS/%s',path,fileName);
-			
+			fileLoad = sprintf('%s%s/%s',path,subdirectory,fileName);
+						
 			% If found break out of the loop
 			if exist(fileLoad,'file') == 2
 				filename = fileLoad;
