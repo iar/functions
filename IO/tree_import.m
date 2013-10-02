3cc31a243d373a45fed3f0fe7780221f718a2251
Michael Hutchins <mlhutch@uw.edu>
2013-10-02 12:29:49 -0700

    Added tree import.

---
+++ IO/tree_import.m
@@ -0,0 +1,67 @@
+function [cluster, tree] = tree_import(date)
+%Imports a tree data file:
+%	cluster - clusters produced by cluster_wwlln
+%	tree - interlinking stormIDs per time segment
+%
+%   Written By:  Michael Hutchins
+
+%% Filepaths
+
+	subdirectory = 'clusterFiles/';
+	prefix = 'tree';
+
+%% Initialize output
+
+	cluster = NaN;
+	tree = NaN;
+	
+%% Check for date specified file
+	if strncmp(class(date),'double',6)
+
+		% Format date into datevec format
+		if length(date) == 1;
+			date=datevec(date);
+			date=date(1:3);
+		elseif length(date)~=3
+			warning('Unknown Input Format');
+		end
+
+		% Generate filename
+		fileName=sprintf('%s%04g%02g%02g.mat',prefix,date(1:3)); 
+
+		% Load dataPath.dat file
+		fid = fopen('dataPath.dat');
+		dataPath = textscan(fid,'%s','Delimiter','\n');
+		dataPath = dataPath{1};
+		
+		% Check each path for the file
+		for i = 1 : size(dataPath,1);
+			
+			% Generate load name to check
+			path = dataPath{i};
+			fileLoad = sprintf('%s%s/%s',path,subdirectory,fileName);
+			
+			% If found break out of the loop
+			if exist(fileLoad,'file') == 2
+				filename = fileLoad;
+				break;
+			end
+			
+			% If loop ends without finding give error
+			if i == size(dataPath,1)
+				error(sprintf('File %s not found!',fileName));
+			end
+		end
+
+%% Check for filename specified file
+	elseif strmatch(class(date),'char')
+		filename=date;
+%% Error out if not found
+	else
+		error('Unrecognized filename.')
+	end
+
+%% Load file
+	load(filename);
+
+end
