bb89b90a3b1bba12ea3b304ed4bc5727f8b70023
Michael Hutchins <mlhutch@uw.edu>
2013-10-02 14:36:06 -0700

    Renamed and slight restructuring.

--- tree_traversal.m
+++ tree_traversal.m
@@ -40,7 +40,7 @@ function [ groups ] = tree_traversal( tree )
 		
 		elements = find(sum(tree == nodes(i),2) == 1);
 		
-		invertedTree{i} = elements;
+		invertedTree{i} = unique(elements);
 		
 	end
 	
@@ -56,39 +56,41 @@ function [ groups ] = traverse(tree, invertedTree)
 
 	groups = zeros(size(tree,1),1);
 
-	%% Traverse tree
+	%% Traverse graph
 	
 	for i = 1 : size(tree,1)
 		
-		entries = tree(i,tree(i,:) > 0);
+		% Get connected IDNodes
+		idNodes = tree(i,tree(i,:) > 0);
 
-		if ~isempty(entries)
-		
-			if groups(i) == 0
-				groups(i) = entries(1);
-			end
-
-			first = groups(i);
-			
-			for j = 1 : length(entries)
-				
-				% Get members to assign groups
-
-				members = invertedTree{entries(j)};
-
-				update = false(length(groups),1);
-				update(members) = true;				
-				
-				% Assign unassigned groups to first
-				groups(update & groups == 0) = first;
-				
-				% Add members to group first
-
-				invertedTree{first} = unique([invertedTree{first}; members]);
-				
-			end
-			
+		% If not connected continue
+		if isempty(idNodes)
+			continue
 		end
+		
+		% If groupID not assigned, set to first IDNode
+		if groups(i) == 0
+			groups(i) = idNodes(1);
+		end
+
+		% Set ID to groupID of node_i
+		ID = groups(i);
+
+		% Go through each connected IDNode
+		for j = 1 : length(idNodes)
+
+			% Get nodes connceted to IDNode
+			nodes = invertedTree{idNodes(j)};
+
+			% Generate logical index from list
+			update = false(length(groups),1);
+			update(nodes) = true;				
+
+			% Assign unassigned IDs to ID of node_i
+			groups(update & groups == 0) = ID;
+
+		end
+			
 		
 	end
 	
