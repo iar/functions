a0040dd51a12ad6d6b4654cbb7791f8f4b5b372c
Michael Hutchins <mlhutch@uw.edu>
2013-10-08 10:22:33 -0700

    Adjusted pairwise plots to give density plots in the lower half and scatter plots in the upper half.

--- pairwise_plot.m
+++ pairwise_plot.m
@@ -1,6 +1,7 @@
 function [ h ] = pairwise_plot( data, names )
-%PAIRWISE_PLOT plots each pair of N variables in data (MxN) in a N x N
-%	subplot figure. Names is a cell array with the names of each variable
+%% PAIRWISE_PLOT plots each pair of N variables in data (MxN) in a N x N
+%	subplot figure. Names is a cell array with the names of each variable.
+%	Upper plots are point plots and lower plots are density plots
 %
 %	Written by: Michael Hutchins
 
@@ -9,9 +10,19 @@ function [ h ] = pairwise_plot( data, names )
 	%% Plotting variables
 
 	N = size(data,2);
-
+	M = size(data,1);
+	
 	nPlot = 1;
 	
+	%% Get plot bounds
+	
+	bounds = {};
+	
+	for i = 1 : N
+		bounds{i} = [min(data(:,i)), max(data(:,i))];
+	end
+		
+		
 	%% Create plot
 	
 	h = figure;
@@ -25,11 +36,30 @@ function [ h ] = pairwise_plot( data, names )
 				
 				subplot(N,N,nPlot)
 				
-				plot(data(:,i),data(:,j),'.')
+				if i < j
 				
-				xlabel(names{i})
-				ylabel(names{j})
-				
+					plot(data(:,i),data(:,j),'.')
+
+					xlim(bounds{i})
+					ylim(bounds{j})
+
+					xlabel(names{i})
+					ylabel(names{j})
+					
+				else
+					
+					xSteps = axisSteps(bounds{i}, floor(M/10));
+					ySteps = axisSteps(bounds{j}, floor(M/10));
+
+					n = hist3([data(:,[i,j])],{xSteps,ySteps});
+					
+					imagesc(xSteps,ySteps,n);
+					set(gca,'YDir','Normal')
+	
+					xlabel(names{j})
+					ylabel(names{i})
+
+				end
 			end
 				
 			nPlot = nPlot + 1;
@@ -39,3 +69,13 @@ function [ h ] = pairwise_plot( data, names )
 	
 end
 
+function steps = axisSteps(bound, n)
+
+	start = bound(1);
+	stop = bound(2);
+	
+	step = (stop - start) / n;
+	
+	steps = start : step : stop;
+
+end
