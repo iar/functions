44956766e557226175d5830766511867f6471259
Michael Hutchins <mlhutch@uw.edu>
2013-12-11 14:53:41 -0800

    Wrote a function to convert WWLLN AE files into JSON formatted files.

---
+++ loc2json.m
@@ -0,0 +1,35 @@
+function loc2json( data, exportName )
+%Exports DATA as an EXPORTNAME.JSON formatted file
+%
+%   Written By:  Michael Hutchins
+
+
+	fid = fopen(sprintf('%s.json',exportName),'wt');
+	fprintf(fid,'{\n');
+
+	for i = 1 : size(data,1);
+
+		stroke = data(i,:);
+		
+		strokeID = i;
+		info = sprintf('%s-%g',exportName,i);
+		unixTime = datenum(stroke(1:6)) - datenum(1970,1,1);
+		unixTime = unixTime * 86400;
+		
+		fprintf(fid,'\t"%i" : {',strokeID);
+		fprintf(fid,'"info" : "%s",\n',info);
+		fprintf(fid,'\t\t"date" : "%g/%g/%g",\n',stroke(1:3));
+		fprintf(fid,'\t\t"time" : "%g:%g:%g UTC",\n',stroke(4:6));
+		fprintf(fid,'\t\t"unixTime" : %.2f,\n',unixTime);
+		fprintf(fid,'\t\t"lat" : %.2f,\n',stroke(7));
+		fprintf(fid,'\t\t"long" : %.2f,\n',stroke(8));
+		fprintf(fid,'\t\t"energy" : %.0f},\n',stroke(11));
+
+		
+	end
+
+	fprintf(fid,'}');
+	
+
+end
+
