12987a85ca8a59816890435c42a251b67eb63c17
Michael Hutchins <mlhutch@uw.edu>
2013-12-17 11:22:54 -0800

    Removed unnecessary json data, changed identifier to be a random number to precent accidental overlap/

--- loc2json.m
+++ loc2json.m
@@ -11,19 +11,14 @@ function loc2json( data, exportName )
 
 		stroke = data(i,:);
 		
-		strokeID = i;
-		info = sprintf('%s-%g',exportName,i);
+		strokeID = 1e7*rand();
 		unixTime = datenum(stroke(1:6)) - datenum(1970,1,1);
 		unixTime = unixTime * 86400;
 		
 		fprintf(fid,'\t"%i" : {',strokeID);
-		fprintf(fid,'"info" : "%s",\n',info);
-		fprintf(fid,'\t\t"date" : "%g/%g/%g",\n',stroke(1:3));
-		fprintf(fid,'\t\t"time" : "%g:%g:%.1f UTC",\n',stroke(4:6));
 		fprintf(fid,'\t\t"unixTime" : %.1f,\n',unixTime);
 		fprintf(fid,'\t\t"lat" : %.2f,\n',stroke(7));
-		fprintf(fid,'\t\t"long" : %.2f,\n',stroke(8));
-		fprintf(fid,'\t\t"energy" : %.0f}',stroke(11));
+		fprintf(fid,'\t\t"long" : %.2f}',stroke(11));
 
 		if i == size(data,1);
 			fprintf(fid,'\n');
